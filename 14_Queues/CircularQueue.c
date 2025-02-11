4af2d6207aa1d5536e08944110ec7dca66867a43
derbuihan <derbuihan@gmail.com>
2025-02-11 16:36:35 +0900

    Refactor Circular Queue implementation for improved readability and consistency

--- 14_Queues/CircularQueue.c
+++ 14_Queues/CircularQueue.c
@@ -1,58 +1,54 @@
 #include "CircularQueue.h"
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 CircularQueue *createCircularQueue(int size) {
-    CircularQueue *cq = (CircularQueue *) malloc(sizeof(CircularQueue));
-    cq->size = size;
-    cq->front = 0;
-    cq->count = 0;
-    cq->arr = (int *) malloc(cq->size * sizeof(int));
-    return cq;
+  CircularQueue *cq = (CircularQueue *)malloc(sizeof(CircularQueue));
+  cq->size = size;
+  cq->front = 0;
+  cq->count = 0;
+  cq->arr = (int *)malloc(cq->size * sizeof(int));
+  return cq;
 }
 
 void freeCircularQueue(CircularQueue *cq) {
-    free(cq->arr);
-    free(cq);
+  free(cq->arr);
+  free(cq);
 }
 
 void pushCircularQueue(CircularQueue *cq, int data) {
-    if (isFullCircularQueue(cq)) {
-        printf("Error: Circular Queue is full\n");
-        exit(1);
-    }
-    int rear = (cq->front + cq->count) % cq->size;
-    cq->arr[rear] = data;
-    cq->count++;
+  if (isFullCircularQueue(cq)) {
+    printf("Error: Circular Queue is full\n");
+    exit(1);
+  }
+  int rear = (cq->front + cq->count) % cq->size;
+  cq->arr[rear] = data;
+  cq->count++;
 }
 
 int popCircularQueue(CircularQueue *cq) {
-    if (isEmptyCircularQueue(cq)) {
-        printf("Error: Circular Queue is empty\n");
-        exit(1);
-    }
+  if (isEmptyCircularQueue(cq)) {
+    printf("Error: Circular Queue is empty\n");
+    exit(1);
+  }
 
-    int val = cq->arr[cq->front];
-    cq->front++;
-    cq->front %= cq->size;
-    cq->count--;
-    return val;
+  int val = cq->arr[cq->front];
+  cq->front++;
+  cq->front %= cq->size;
+  cq->count--;
+  return val;
 }
 
-int isFullCircularQueue(CircularQueue *cq) {
-    return cq->count == cq->size;
-}
+int isFullCircularQueue(CircularQueue *cq) { return cq->count == cq->size; }
 
-int isEmptyCircularQueue(CircularQueue *cq) {
-    return cq->count == 0;
-}
+int isEmptyCircularQueue(CircularQueue *cq) { return cq->count == 0; }
 
 void displayCircularQueue(CircularQueue *cq) {
-    printf("[ ");
-    for (int i = 0; i < cq->count; i++) {
-        int idx = (cq->front + i) % cq->size;
-        printf("%d ", cq->arr[idx]);
-    }
-    printf("]\n");
+  printf("[ ");
+  for (int i = 0; i < cq->count; i++) {
+    int idx = (cq->front + i) % cq->size;
+    printf("%d ", cq->arr[idx]);
+  }
+  printf("]\n");
 }
--- 14_Queues/CircularQueue.h
+++ 14_Queues/CircularQueue.h
@@ -4,25 +4,18 @@
 typedef struct CircularQueue CircularQueue;
 
 struct CircularQueue {
-    int size;
-    int front;
-    int count;
-    int *arr;
+  int size;
+  int front;
+  int count;
+  int *arr;
 };
 
 CircularQueue *createCircularQueue(int size);
-
 void freeCircularQueue(CircularQueue *cq);
-
 void pushCircularQueue(CircularQueue *cq, int data);
-
 int popCircularQueue(CircularQueue *cq);
-
 int isFullCircularQueue(CircularQueue *cq);
-
 int isEmptyCircularQueue(CircularQueue *cq);
-
 void displayCircularQueue(CircularQueue *cq);
 
-
-#endif //CIRCULARQUEUE_H
+#endif  // CIRCULARQUEUE_H
